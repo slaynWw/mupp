2dfe81a5d7b0420f76b17784e17d045000587413
slxnqq <neon73neon@gmail.com>
2025-04-04 00:43:32 +0300

    0.0.1

--- src/commands/client/utility/status.js
+++ src/commands/client/utility/status.js
@@ -7,6 +7,7 @@ module.exports = {
             {newEmbed:
                 {description:-# ./commands/utilities}
                 {field:/ChalresNaig.node:\`\`\`json\nstatus\: $nodeInfo[CharlesNaig;status]\nplayers\: $nodeInfo[CharlesNaig;player.used]/$nodeInfo[CharlesNaig;player.total]\`\`\`:false}
+                {field:/Ajieblogs.node:\`\`\`json\nstatus\: $nodeInfo[Ajieblogs;status]\nplayers\: $nodeInfo[Ajieblogs;player.used]/$nodeInfo[Ajieblogs;player.total]\`\`\`:false}
                 {field:/readme.txt:На данный момент в боте используются только\nбесплатные общедоступные ноды.:false}
                 {image:$getImage[mupp.status]}
                 {color:$getData[embed.color]}
