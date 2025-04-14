a42fdc6635e6763bacce1c838df1fa06f6265634
slxnqq <neon73neon@gmail.com>
2025-04-15 01:57:38 +0300

    0.1

--- src/commands/client/music/previous.js
+++ src/commands/client/music/previous.js
@@ -10,6 +10,7 @@ module.exports = {
             $resumeTrack
         $endIf
         $previousTrack
+        $onlyIf[$loopStatus==off;Упс... Что-то пошло не так...\n-# Отключите повтор трека/очереди что-бы вернуться на прошлый трек.{options:{interaction}}{extraOptions:{ephemeral}}]
         $onlyIf[$voiceID[$authorId]==$voiceID[$clientID];Упс... Что-то пошло не так...\n-# Что-бы использовать меню, подключитесь к голосовому каналу в котором находится бот.{options:{interaction}}{extraOptions:{ephemeral}}]
     `
 }
